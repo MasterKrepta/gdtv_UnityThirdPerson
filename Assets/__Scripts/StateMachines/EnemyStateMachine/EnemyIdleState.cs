7ed1e69aaa4b181b51ef6c9ddb910034dca74d2a
Brian <brianj.pajak@gmail.com>
2022-06-11 10:52:35 -0400

    Enemy Base State

---
+++ Assets/__Scripts/StateMachines/EnemyStateMachine/EnemyIdleState.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyIdleState : EnemyBaseState
+{
+    readonly int LOCOMOTION_HASH = Animator.StringToHash("Locomotion");
+    readonly int SPEED_HASH = Animator.StringToHash("Speed");
+
+    private const float CROSSFADE_DUR = 0.1F;
+    private const float ANIM_DAMP_TIME = 0.1F;
+    public EnemyIdleState(EnemyStateMachine stateMachine) : base(stateMachine) { }
+
+    public override void Enter()
+    {
+        _stateMachine.Anim.CrossFadeInFixedTime(LOCOMOTION_HASH, CROSSFADE_DUR);
+        
+    }
+
+    public override void Tick(float deltaTime)
+    {
+        _stateMachine.Anim.SetFloat(SPEED_HASH, 0f, ANIM_DAMP_TIME, deltaTime);
+    }
+
+    public override void Exit()  { }
+
+
+
+}
+
---
+++ Assets/__Scripts/StateMachines/PlayerStateMachine/PlayerBaseState.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class PlayerBaseState : State
+{
+    protected PlayerStateMachine _stateMachine;
+
+
+    public PlayerBaseState(PlayerStateMachine stateMachine)
+    {
+        _stateMachine = stateMachine;
+    }
+
+    protected void Move(Vector3 motion, float deltaTime)
+    {
+        _stateMachine.Controller.Move((motion + _stateMachine.ForceReceiver.Movement) * deltaTime);
+    }
+
+    protected void Move(float deltaTime)
+    {
+        Move(Vector3.zero, deltaTime);
+    }
+
+    protected void FaceTarget()
+    {
+        if (_stateMachine.Targeter.CurrentTarget == null) return;
+
+        Vector3 lookDir = (_stateMachine.Targeter.CurrentTarget.transform.position - _stateMachine.transform.position);
+        lookDir.y = 0;
+
+        _stateMachine.transform.rotation = Quaternion.LookRotation(lookDir);
+        
+
+    }
+}
